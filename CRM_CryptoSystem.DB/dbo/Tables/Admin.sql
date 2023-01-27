4e511a391a8504d8bb0c694e2a159b2ea5e4bb46
Ilya <ilia.il.27@mail.ru>
2023-01-27 20:53:51 +0300

    import DB

---
+++ CRM_CryptoSystem.DB/dbo/Tables/Admin.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Admin] (
+    [Id]        BIGINT     NOT NULL,
+    [Email]     NCHAR (50) NOT NULL,
+    [Password]  NCHAR (25) NOT NULL,
+    [IsDeleted] BIT        NOT NULL
+);
+
