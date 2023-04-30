03e74497a6762e5d59f43f1fc0aaff3a65c82f1f
Ilya <ilia.il.27@mail.ru>
2023-04-30 18:01:43 +0300

    Add methods remove & restore

--- CRM_CryptoSystem.API/Controllers/LeadsController.cs
+++ CRM_CryptoSystem.API/Controllers/LeadsController.cs
@@ -74,4 +74,56 @@ public class LeadsController : ControllerBase
         var leads = new List<LeadMainInfoResponse>();
         return Ok(leads);
     }
+
+    [Authorize]
+    [HttpPut("{id}")]
+    [ProducesResponseType(typeof(void), StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status422UnprocessableEntity)]
+    public async Task<ActionResult> Update([FromBody] LeadUpdateRequest request, int id)
+    {
+        _logger.LogInformation($"Controller: Update lead by id: {id}: {request.FirstName}, {request.LastName}, {request.Patronymic}, {request.Birthday}, {request.Phone}");
+        //var claims = this.GetClaims();
+
+        //await _leadsService.Update(_mapper.Map<LeadDto>(request), id, claims);
+        return NoContent();
+    }
+
+    [Authorize]
+    [HttpDelete("{id}")]
+    [ProducesResponseType(typeof(void), StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult> Remove(int id)
+    {
+        //var claims = this.GetClaims();
+
+        //var lead = await _leadsService.GetById(id, claims);
+       /* _logger.LogInformation($"Controller: Remove lead by id {id}:{lead.FirstName}, {lead.LastName}, {lead.Patronymic}, {lead.Birthday}, {lead.Phone.MaskNumber()}, " +
+            $"{lead.City}, {lead.Address.MaskTheLastFive}, {lead.Email.MaskEmail()}, {lead.Passport.MaskPassport()}");*/
+        //await _leadsService.Delete(id, true, claims);
+
+        return NoContent();
+    }
+
+    [AuthorizeByRole(Role.Admin)]
+    [HttpPatch("{id}/restore")]
+    [ProducesResponseType(typeof(void), StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult> Restore(int id)
+    {
+        /*var claims = this.GetClaims();
+        var lead = await _leadsService.GetDeletedLeadById(id, claims);
+        _logger.LogInformation($"Controller: Restore lead by id {id}: {lead.FirstName}, {lead.LastName}, {lead.Patronymic}, {lead.Birthday}, {lead.Phone.MaskNumber()}, " +
+            $"{lead.City}, {lead.Address.MaskTheLastFive}, {lead.Email.MaskEmail()}, {lead.Passport.MaskPassport()}");
+        await _leadsService.Restore(id, false, claims);*/
+
+        return NoContent();
+    }
 }
+
