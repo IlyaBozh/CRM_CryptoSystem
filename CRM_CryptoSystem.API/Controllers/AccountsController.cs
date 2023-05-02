86378badc4534625dc1130699260945c7fda4c9b
Ilya <ilia.il.27@mail.ru>
2023-05-03 00:04:33 +0300

    Add AccountController

---
+++ CRM_CryptoSystem.API/Controllers/AccountsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRM_CryptoSystem.API.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
