d4492b590dc7766e3c58f75eaf491b909742515a
Ilya <ilia.il.27@mail.ru>
2023-05-03 00:06:23 +0300

    Add method GetAccount

--- CRM_CryptoSystem.API/Controllers/AccountsController.cs
+++ CRM_CryptoSystem.API/Controllers/AccountsController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CRM_CryptoSystem.API.Models.Responses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-namespace CRM_CryptoSystem.API.Controllers
+namespace CRM_CryptoSystem.API.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class AccountsController : Controller
 {
-    public class AccountsController : Controller
+    /*private readonly IAccountService _accountService;*/
+    private readonly IMapper _mapper;
+    private readonly ILogger<AccountsController> _logger;
+
+    public AccountsController (IMapper mapper, ILogger<AccountsController> logger)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _mapper = mapper;
+        _logger = logger;
     }
+
+    [Authorize]
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(AccountResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<AccountResponse>> GetAccount(int id)
+    {
+        _logger.LogInformation($"Controller: Get an account by id {id}");
+/*        var claim = this.GetClaims();
+        var result = await _accountService.GetAccountById(id, claim);*/
+        var result = new AccountResponse();
+        if (result == null)
+            return NotFound();
+        else
+            /*return Ok(_mapper.Map<AccountResponse>(result));*/
+            return Ok(result);
+    }
+
+
 }
