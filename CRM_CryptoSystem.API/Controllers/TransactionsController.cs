acc5db7e2980f98d2cfe090c203aedbd53aec848
Ilya <ilia.il.27@mail.ru>
2023-05-03 00:25:51 +0300

    Add TransactionsController

---
+++ CRM_CryptoSystem.API/Controllers/TransactionsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRM_CryptoSystem.API.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+[Route("[controller]")]
+public class TransactionsController : Controller
+{
+    /*public ClaimModel _claims;*/
+    /*private readonly ITransactionsService _transactionsService;*/
+    private readonly ILogger<TransactionsController> _logger;
+
+    public TransactionsController(ILogger<TransactionsController> logger)
+    {
+        _logger = logger;
+    }
+
+
+}
