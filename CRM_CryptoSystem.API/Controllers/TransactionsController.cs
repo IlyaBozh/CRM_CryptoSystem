9f75a206ff21d3f4c1ca15a5221c1caeecf6a433
Ilya <ilia.il.27@mail.ru>
2023-05-03 00:30:57 +0300

    Add TransactionRequest & add method AddDeposit

--- CRM_CryptoSystem.API/Controllers/TransactionsController.cs
+++ CRM_CryptoSystem.API/Controllers/TransactionsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRM_CryptoSystem.API.Models.Requests;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CRM_CryptoSystem.API.Controllers;
 
@@ -16,5 +18,18 @@ public class TransactionsController : Controller
         _logger = logger;
     }
 
-
+    [Authorize]
+    [HttpPost("deposit")]
+    [ProducesResponseType(typeof(long), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status422UnprocessableEntity)]
+    public async Task<ActionResult<long>> AddDeposit([FromBody] TransactionRequest request)
+    {
+        _logger.LogInformation("Controllers: Add deposit");
+        /*var claims = this.GetClaims();
+        var transactionId = await _transactionsService.AddDeposit(request);
+        return Created($"{this.GetShemeAndHostString()}/transactions/{transactionId}", transactionId);*/
+        return Created("1", 1);
+    }
 }
