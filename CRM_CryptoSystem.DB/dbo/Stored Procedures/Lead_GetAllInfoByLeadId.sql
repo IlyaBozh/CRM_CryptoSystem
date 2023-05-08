9c61668ef6f7e8b6c04ebafe62c71e12d85ed33a
Ilya <ilia.il.27@mail.ru>
2023-05-08 19:15:12 +0300

    Add Stored Procedure Lead_GetAllInfoByLeadId

---
+++ CRM_CryptoSystem.DB/dbo/Stored Procedures/Lead_GetAllInfoByLeadId.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[Lead_GetAllInfoByLeadId]
+	@Id int
+AS
+BEGIN
+
+SELECT
+	
+		L.Id, 
+		L.FirstName, 
+		L.LastName, 
+		L.Patronymic, 
+		L.Birthday, 
+		L.Email, 
+		L.Phone,
+		L.[Login], 
+		L.[Role], 
+		L.RegistrationDate, 
+		A.Id, 
+		A.CryptoCurrency, 
+		A.[Status] 
+	FROM [dbo].[Lead] AS L
+	LEFT JOIN [dbo].[Account] AS A ON L.Id = A.LeadId
+
+	WHERE L.Id = @Id AND L.IsDeleted = 0 
+END
