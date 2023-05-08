496910d85149ce78ac29378e013bb9f12da5159b
Ilya <ilia.il.27@mail.ru>
2023-05-08 19:10:57 +0300

    Add Stored Procedure Lead_GetAll

---
+++ CRM_CryptoSystem.DB/dbo/Stored Procedures/Lead_GetAll.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[Lead_GetAll]
+
+AS
+	SELECT 
+			Id, 
+			FirstName, 
+			LastName, 
+			Patronymic, 
+			Birthday, 
+			Email, 
+			Phone,
+			Passport, 
+			City, 
+			[Address], 
+			[Role], 
+			RegistrationDate
+	FROM dbo.[Lead]
+	WHERE (IsDeleted = 0)
+
+END
