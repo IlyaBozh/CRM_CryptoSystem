600d5ee3a662c8ab6045b44282b5915ce7cbd5dd
Ilya <ilia.il.27@mail.ru>
2023-05-08 23:52:00 +0300

    Add & implement interface ILeadsRepository

--- CRM_CryptoSystem.DataLayer/Interfaces/ILeadsRepository.cs
+++ CRM_CryptoSystem.DataLayer/Interfaces/ILeadsRepository.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CRM_CryptoSystem.DataLayer.Models;
 
 namespace CRM_CryptoSystem.DataLayer.Interfaces;
 
-public class ILeadsRepository
-{ 
+public interface ILeadsRepository
+{
+    Task<int> Add(LeadDto leadDto);
+    Task DeleteOrRestore(int id, bool isDeleting);
+    Task<List<LeadDto>> GetAll();
+    Task<LeadDto> GetAllInfoById(int id);
+    Task<LeadDto> GetByEmail(string email);
+    Task<LeadDto> GetById(int id);
+    Task Update(LeadDto leadDto);
 }
---
+++ CRM_CryptoSystem.DataLayer/Repositories/LeadsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_CryptoSystem.DataLayer.Repositories;
+
+public class LeadsRepository
+{
+}
