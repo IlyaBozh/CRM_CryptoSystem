f68e641c1a46f8bba2d0228ab16c49f3f35e4ffd
Ilya <ilia.il.27@mail.ru>
2023-05-09 16:31:10 +0300

    Add Stored Procedure Account_Add

---
+++ CRM_CryptoSystem.DB/dbo/Stored Procedures/Account_Add.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[Account_Add]
+	@CryptoCurrency tinyint,
+	@Status tinyint,
+	@LeadId int
+AS
+BEGIN
+	INSERT INTO dbo.[Account](
+			CryptoCurrency,
+			[Status],
+			LeadId)
+	VALUES(
+			@CryptoCurrency,
+			@Status,
+			@LeadId)
+
+	SELECT @@IDENTITY
+END
+
