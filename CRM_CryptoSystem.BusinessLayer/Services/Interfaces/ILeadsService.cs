45d9a26c3b20d884c6170ed1e7d6e59887a25d42
Ilya <ilia.il.27@mail.ru>
2023-05-09 16:18:55 +0300

    Fill & implement interface

--- CRM_CryptoSystem.BusinessLayer/Services/Interfaces/ILeadsService.cs
+++ CRM_CryptoSystem.BusinessLayer/Services/Interfaces/ILeadsService.cs
@@ -1,7 +1,16 @@
 ﻿
+using CRM_CryptoSystem.BusinessLayer.Models;
+using CRM_CryptoSystem.DataLayer.Models;
 
 namespace CRM_CryptoSystem.BusinessLayer.Services.Interfaces;
 
 public interface ILeadsService
 {
+    Task<int> Add(LeadDto lead);
+    Task<LeadDto> DeleteOrRestore(int id, bool isDeleted, ClaimModel claims);
+    Task<List<LeadDto>> GetAll();
+    Task<LeadDto> GetAllInfoById(int id, ClaimModel claims);
+    Task<LeadDto?> GetByEmail(string email);
+    Task<LeadDto> GetById(int id, ClaimModel claims);
+    Task Update(LeadDto lead, int id, ClaimModel claims);
 }
--- CRM_CryptoSystem.BusinessLayer/Services/LeadsService.cs
+++ CRM_CryptoSystem.BusinessLayer/Services/LeadsService.cs
@@ -1,8 +1,45 @@
-﻿using CRM_CryptoSystem.BusinessLayer.Services.Interfaces;
+﻿using CRM_CryptoSystem.BusinessLayer.Models;
+using CRM_CryptoSystem.BusinessLayer.Services.Interfaces;
+using CRM_CryptoSystem.DataLayer.Interfaces;
+using CRM_CryptoSystem.DataLayer.Models;
 
 namespace CRM_CryptoSystem.BusinessLayer.Services;
 
 public class LeadsService : ILeadsService
 {
+    private readonly ILeadsRepository _leadsRepository;
+    public Task<int> Add(LeadDto lead)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<LeadDto> DeleteOrRestore(int id, bool isDeleted, ClaimModel claims)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<LeadDto>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LeadDto> GetAllInfoById(int id, ClaimModel claims)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LeadDto?> GetByEmail(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<LeadDto> GetById(int id, ClaimModel claims)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(LeadDto lead, int id, ClaimModel claims)
+    {
+        throw new NotImplementedException();
+    }
 }
