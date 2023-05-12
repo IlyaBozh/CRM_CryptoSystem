8a22442fbdcee277c1035c77739d1eaac952760d
Ilya <ilia.il.27@mail.ru>
2023-05-12 17:19:26 +0300

    Add Constructor

--- CRM_CryptoSystem.BusinessLayer/Services/AuthService.cs
+++ CRM_CryptoSystem.BusinessLayer/Services/AuthService.cs
@@ -1,11 +1,24 @@
 ﻿
 using CRM_CryptoSystem.BusinessLayer.Models;
 using CRM_CryptoSystem.BusinessLayer.Services.Interfaces;
+using CRM_CryptoSystem.DataLayer.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace CRM_CryptoSystem.BusinessLayer.Services;
 
 public class AuthService : IAuthService
 {
+    private readonly ILeadsRepository _leadsRepository;
+    private readonly IAdminsRepository _adminsRepository;
+    private readonly ILogger<AuthService> _logger;
+
+    public AuthService(ILeadsRepository leadsRepository, IAdminsRepository adminsRepository, ILogger<AuthService> logger)
+    {
+        _leadsRepository = leadsRepository;
+        _adminsRepository = adminsRepository;
+        _logger = logger;
+    }
+
     public Task<string> GetToken(ClaimModel claim)
     {
         throw new NotImplementedException();
