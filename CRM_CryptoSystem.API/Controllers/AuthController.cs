079f3cd01b7f1bdde78e433b5d94463c7403f30d
Ilya <ilia.il.27@mail.ru>
2023-05-12 18:32:16 +0300

    Add method Login

--- CRM_CryptoSystem.API/Controllers/AuthController.cs
+++ CRM_CryptoSystem.API/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CRM_CryptoSystem.API.Models.Requests;
+using CRM_CryptoSystem.BusinessLayer.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CRM_CryptoSystem.API.Controllers;
@@ -8,5 +10,23 @@ namespace CRM_CryptoSystem.API.Controllers;
 [Route("[controller]")]
 public class AuthController : ControllerBase
 {
-    
+    private readonly IAuthService _authService;
+    private readonly ILogger<AuthController> _logger;
+
+    public AuthController(IAuthService authService, ILogger<AuthController> logger)
+    {
+        _authService = authService;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    public async Task<ActionResult<string>> Login([FromBody] LoginRequest loginRequest)
+    {
+        var user = await _authService.Login(loginRequest.Email, loginRequest.Password);
+
+        _logger.LogInformation($"Controllers: Login is successful for {loginRequest.Email}");
+
+        return Ok(_authService.GetToken(user));
+    }
 }
