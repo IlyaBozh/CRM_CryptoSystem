66760a37a6452398c48b07dc5bb6e8c6750a55b4
Ilya <ilia.il.27@mail.ru>
2023-05-12 16:50:44 +0300

    Add AuthController

---
+++ CRM_CryptoSystem.API/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM_CryptoSystem.API.Controllers;
+
+[AllowAnonymous]
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    
+}
