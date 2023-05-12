78e47b9ccf3de923e3d56a5b67a5b8326d0074f3
Ilya <ilia.il.27@mail.ru>
2023-05-12 16:04:46 +0300

    Add interface IAccountsService

---
+++ CRM_CryptoSystem.BusinessLayer/Services/Interfaces/IAccountsService.cs
@@ -0,0 +1,14 @@
+﻿
+using CRM_CryptoSystem.BusinessLayer.Models;
+using CRM_CryptoSystem.DataLayer.Models;
+
+namespace CRM_CryptoSystem.BusinessLayer.Services.Interfaces;
+
+public interface IAccountsService
+{
+    public Task<int> Add(AccountDto accountDTO, ClaimModel claim);
+    public Task DeleteOrRestore(int id, ClaimModel claim);
+    public Task<List<AccountDto>> GetAllByLeadId(int id, ClaimModel claim);
+    public Task<AccountDto> GetById(int id, ClaimModel calim);
+    public Task Update(AccountDto accountDTO, int id, ClaimModel claim);
+}
