1247b37c18d5b9da7008cc628168515f0d71892d
Ilya <ilia.il.27@mail.ru>
2023-05-12 15:28:06 +0300

    create ectinsion for string & add method MaskEmail

---
+++ CRM_CryptoSystem.BusinessLayer/Infrastructure/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CRM_CryptoSystem.BusinessLayer.Infrastructure;
+
+public static class StringExtension
+{
+    public static string MaskEmail(this string original)
+    {
+        int index = original.IndexOf('@');
+        string output = original;
+
+        if (index > 3)
+            output = original.Substring(0, 2) + new string('*', index - 2) + original.Substring(index);
+        
+        return output;
+    }
+}
