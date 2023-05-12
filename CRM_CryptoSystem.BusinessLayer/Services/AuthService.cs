6c8c97c1a9e3649b62a1011e948d1dffdd0cca97
Ilya <ilia.il.27@mail.ru>
2023-05-12 16:55:35 +0300

    Create & im[lement IAuthService, Creat class AuthService

---
+++ CRM_CryptoSystem.BusinessLayer/Services/AuthService.cs
@@ -0,0 +1,18 @@
+﻿
+using CRM_CryptoSystem.BusinessLayer.Models;
+using CRM_CryptoSystem.BusinessLayer.Services.Interfaces;
+
+namespace CRM_CryptoSystem.BusinessLayer.Services;
+
+public class AuthService : IAuthService
+{
+    public Task<string> GetToken(ClaimModel claim)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ClaimModel> Login(string login, string password)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CRM_CryptoSystem.BusinessLayer/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿
+using CRM_CryptoSystem.BusinessLayer.Models;
+
+namespace CRM_CryptoSystem.BusinessLayer.Services.Interfaces;
+
+public interface IAuthService
+{
+    public Task<ClaimModel> Login(string login, string password);
+    public Task<string> GetToken(ClaimModel claim);
+}
