bd4d0b7fbf33c1a590328982100d341c9817bb52
Ilya <ilia.il.27@mail.ru>
2023-05-12 15:12:35 +0300

    Add ErrorResult & ExceptionMiddleware

---
+++ CRM_CryptoSystem.BusinessLayer/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,42 @@
+﻿
+using CRM_CryptoSystem.BusinessLayer.Exceptions;
+using CRM_CryptoSystem.BusinessLayer.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace CRM_CryptoSystem.BusinessLayer.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (NotFoundException error)
+        {
+            await HandleExceptionAsync(httpContext, HttpStatusCode.NotFound, error.Message);
+        }
+
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, HttpStatusCode statusCode, string message)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)statusCode;
+
+        await context.Response.WriteAsync(new ErrorResult()
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = message
+        }.ToString());
+    }
+}
