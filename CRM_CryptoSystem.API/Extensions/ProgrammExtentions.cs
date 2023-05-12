af86c18dd0f42198964c86af335cf0177f3deb62
Ilya <ilia.il.27@mail.ru>
2023-05-12 18:36:42 +0300

    Add static method AddSwaggerGen

--- CRM_CryptoSystem.API/Extensions/ProgrammExtentions.cs
+++ CRM_CryptoSystem.API/Extensions/ProgrammExtentions.cs
@@ -1,6 +1,38 @@
-﻿namespace CRM_CryptoSystem.API.Extensions;
+﻿using Microsoft.OpenApi.Models;
+
+namespace CRM_CryptoSystem.API.Extensions;
 
 public static class ProgrammExtentions
 {
+    public static void AddSwaggerGen(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "CRM", Version = "v1" });
 
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "Authorization: Bearer JWT",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                BearerFormat = "JWT",
+                Scheme = "Bearer",
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer",
+                 },
+                },
+                    Array.Empty<string>()
+                },
+            });
+        });
+    }
 }
