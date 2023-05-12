4f99a1bfdcd695f199b07178ab72428ce00b00f5
Ilya <ilia.il.27@mail.ru>
2023-05-12 17:12:24 +0300

    Add class AdminsRepository & implement interface IAdminsRepository

---
+++ CRM_CryptoSystem.DataLayer/Repositories/AdminsRepository.cs
@@ -0,0 +1,26 @@
+﻿
+using CRM_CryptoSystem.DataLayer.Interfaces;
+using CRM_CryptoSystem.DataLayer.Models;
+using Microsoft.Extensions.Logging;
+using System.Data;
+
+namespace CRM_CryptoSystem.DataLayer.Repositories;
+
+public class AdminsRepository : BaseRepository, IAdminsRepository
+{
+    private readonly ILogger<AdminsRepository> _logger;
+    public AdminsRepository(IDbConnection dbConnection, ILogger<AdminsRepository> logger) : base(dbConnection)
+    {
+        _logger = logger;
+    }
+
+    public Task<int> AddAdmin(AdminDto admin)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AdminDto> GetAdminByEmail(string email)
+    {
+        throw new NotImplementedException();
+    }
+}
