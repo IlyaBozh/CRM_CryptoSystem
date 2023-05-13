7bb83b89c2f35c6bff8309a1d936c7430a5e8bbc
Ilya <ilia.il.27@mail.ru>
2023-05-13 12:28:06 +0300

    Add MapperConfig

---
+++ CRM_CryptoSystem.API/Infastructure/MapperConfig.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using CRM_CryptoSystem.API.Models.Requests;
+using CRM_CryptoSystem.API.Models.Responses;
+using CRM_CryptoSystem.DataLayer.Models;
+
+namespace CRM_CryptoSystem.API.Infastructure;
+
+public class MapperConfig : Profile 
+{
+    public MapperConfig()
+    {
+        CreateMap<LeadRegistrationRequest, LeadDto>();
+        CreateMap<LeadUpdateRequest, LeadDto>();
+        CreateMap<LeadDto, LeadAllInfoResponse>();
+        CreateMap<LeadDto, LeadMainInfoResponse>();
+
+        CreateMap<AddAccountRequest, AccountDto>();
+        CreateMap<AddAccountRequest, UpdateAccountRequest>();
+        CreateMap<UpdateAccountRequest, AddAccountRequest>();
+        CreateMap<AccountDto, AddAccountRequest>();
+        CreateMap<UpdateAccountRequest, AccountDto>();
+        CreateMap<AccountDto, AccountResponse>();
+        CreateMap<LoginRequest, AdminDto>()
+            .ForMember(l => l.Email, s => s.MapFrom(a => a.Email))
+            .ForMember(l => l.Password, s => s.MapFrom(a => a.Password));
+
+    }
+}
