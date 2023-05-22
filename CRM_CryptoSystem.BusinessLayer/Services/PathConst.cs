c3e15882639772e66a66454bf3b9eb395300e402
Ilya <ilia.il.27@mail.ru>
2023-05-23 01:58:02 +0300

    Add PathConst

---
+++ CRM_CryptoSystem.BusinessLayer/Services/PathConst.cs
@@ -0,0 +1,9 @@
+﻿
+namespace CRM_CryptoSystem.BusinessLayer.Services;
+
+public class PathConst
+{
+    public const string DepositPath = "/transactions/deposit";
+
+    public const string WithdrawPath = "/transactions/withdraw";
+}
