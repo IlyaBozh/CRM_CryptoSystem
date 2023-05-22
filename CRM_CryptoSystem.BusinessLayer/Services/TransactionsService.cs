a329ce15081f16b8bfeb8324ed92f8b136dee923
Ilya <ilia.il.27@mail.ru>
2023-05-23 02:38:24 +0300

    Fill service TransactionsService

--- CRM_CryptoSystem.BusinessLayer/Services/TransactionsService.cs
+++ CRM_CryptoSystem.BusinessLayer/Services/TransactionsService.cs
@@ -2,33 +2,49 @@
 using CRM_CryptoSystem.BusinessLayer.Services.Interfaces;
 using IncredibleBackendContracts.Requests;
 using IncredibleBackendContracts.Responses;
+using Microsoft.Extensions.Logging;
 
 namespace CRM_CryptoSystem.BusinessLayer.Services;
 
 public class TransactionsService : ITransactionsService
 {
-    public Task<long> AddDeposit(TransactionRequest request)
+    private readonly IHttpService _httpService;
+    private readonly ILogger<TransactionsService> _logger;
+    public TransactionsService(IHttpService httpService, ILogger<TransactionsService> logger)
     {
-        throw new NotImplementedException();
+        _httpService = httpService;
+        _logger = logger;
     }
 
-    public Task<long> AddWithdraw(TransactionRequest request)
+    public async Task<long> AddDeposit(TransactionRequest request)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation($"Business layer: Database query for adding deposit: {request.AccountId}, {request.Amount}, {request.Currency}");
+        return await _httpService.Post<TransactionRequest, long>(request, PathConst.DepositPath);
     }
 
-    public Task<decimal> GetBalanceByAccountsId(int accountId)
+    public async Task<long> AddWithdraw(TransactionRequest request)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation($"Business layer: Database query for adding withdraw {request.AccountId}, {request.Amount}, {request.Currency}");
+        return await _httpService.Post<TransactionRequest, long>(request, PathConst.WithdrawPath);
     }
 
-    public Task<TransactionResponse> GetTransactionById(int transactionId)
+    public async Task<decimal> GetBalanceByAccountsId(int accountId)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation($"Business layer: Database query for getting balance by accounts id {accountId}");
+        return await _httpService.GetBalanceByAccountsId(accountId);
     }
 
-    public Task<List<TransactionResponse>> GetTransactionsByAccountId(int accountId)
+    public async Task<TransactionResponse> GetTransactionById(int transactionId)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation($"Business layer: Database query for getting transaction by id {transactionId}");
+        var content = await _httpService.GetTransaction(transactionId);
+        return content;
+    }
+
+    public async Task<List<TransactionResponse>> GetTransactionsByAccountId(int accountId)
+    {
+        _logger.LogInformation($"Business layer: Database query for getting transactions by account id {accountId}");
+        var content = await _httpService.GetTransactionsByAccountId(accountId);
+        return content;
     }
 }
