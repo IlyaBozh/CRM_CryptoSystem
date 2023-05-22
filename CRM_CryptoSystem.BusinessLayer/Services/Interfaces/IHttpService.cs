6e335a022094ce46e03b329269188a1a92c1f765
Ilya <ilia.il.27@mail.ru>
2023-05-23 02:04:52 +0300

    Add IHttpService

---
+++ CRM_CryptoSystem.BusinessLayer/Services/Interfaces/IHttpService.cs
@@ -0,0 +1,13 @@
+﻿
+using IncredibleBackendContracts.Responses;
+
+namespace CRM_CryptoSystem.BusinessLayer.Services.Interfaces;
+
+public interface IHttpService
+{
+    Task<K> Post<T, K>(T payload, string path);
+    Task<TransactionResponse> GetTransaction(int transactionId);
+    Task<List<TransactionResponse>> GetTransactionsByAccountId(int accountId);
+
+    Task<decimal> GetBalanceByAccountsId(int accountId);
+}
