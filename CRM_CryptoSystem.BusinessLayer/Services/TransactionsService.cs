5b4ad74ddda58c447c1e35a4de4ff2baba4d9d81
Ilya <ilia.il.27@mail.ru>
2023-05-23 02:35:01 +0300

    Add & implement interface ITransactionsService

---
+++ CRM_CryptoSystem.BusinessLayer/Services/TransactionsService.cs
@@ -0,0 +1,34 @@
+﻿
+using CRM_CryptoSystem.BusinessLayer.Services.Interfaces;
+using IncredibleBackendContracts.Requests;
+using IncredibleBackendContracts.Responses;
+
+namespace CRM_CryptoSystem.BusinessLayer.Services;
+
+public class TransactionsService : ITransactionsService
+{
+    public Task<long> AddDeposit(TransactionRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<long> AddWithdraw(TransactionRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<decimal> GetBalanceByAccountsId(int accountId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TransactionResponse> GetTransactionById(int transactionId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<TransactionResponse>> GetTransactionsByAccountId(int accountId)
+    {
+        throw new NotImplementedException();
+    }
+}
